bab9ee6ea1ae037752bff1428a032c2b382509df
Fricky123 <renzosasil@gmail.com>
2022-08-06 08:23:48 +0800

    changed stuff

--- Food Menu/FoodMenu.py
+++ Food Menu/FoodMenu.py
@@ -1,6 +1,8 @@
 import random
+import locale
+locale.setlocale(locale.LC_ALL, '')
 
-"""Online Order simulation"""
+"""Online Order Simulator"""
 
 # ----------------------------------------------------------------------
 '''global variables'''
@@ -8,7 +10,7 @@ import random
 # Cart
 Costs = 0
 Items = 0
-List = []
+List = ""
 Customer_Money = random.randint(500, 1000)
 
 # Food
@@ -56,17 +58,20 @@ def menu():
     separator_line()
     customer_cart()
 
-    print("Options: [1]Food Menu, [2]Purchase/Reset Cart, [3] Exit")
+    if Items:
+        print("\n[1]Food Menu [2]Purchase/Reset Cart [e] Exit")
+    else:
+        print("[1]Food Menu [e] Exit")
 
     x = input("Choose option: ")
 
     if x == "1":
-        ordering()
+        food_menu()
 
     elif x == "2":
         cart_menu()
 
-    elif x == "3":
+    elif x == "e":
         separator_line()
         y = input("Exit to Main Menu[y/n]: ")
 
@@ -84,106 +89,6 @@ def menu():
         invalid_input()
         menu()
 
-def ordering():
-    """Provides customer with a list of choices for food"""
-
-    separator_line()
-
-    print(f"\n"
-          f"[1]{f.get('f1')} = {f_p.get('f1')} php\n"
-          f"[2]{f.get('f2')} = {f_p.get('f2')} php\n"
-          f"[3]{f.get('f3')} = {f_p.get('f3')} php\n"
-          f"[4]{f.get('f4')} = {f_p.get('f4')} php\n"
-          f"[5]{f.get('f5')} = {f_p.get('f5')} php\n"
-          f"[6]Exit\n")
-
-    x = input("Input order: ")
-
-    if x == "1":
-        transaction("f1")
-    elif x == "2":
-        transaction("f2")
-    elif x == "3":
-        transaction("f3")
-    elif x == "4":
-        transaction("f4")
-    elif x == "5":
-        transaction("f5")
-    elif x == "6":
-        print("(Exiting Food Menu...)")
-        menu()
-
-    else:
-        invalid_input()
-        ordering()
-
-def transaction(x):
-    """Make changes to COSTS/ITEMS/LIST in customer's CART based on his order"""
-    separator_line()
-
-    global Costs
-    global Items
-
-    print(f"{f.get(x)} selected...")
-
-    separator_line()
-
-    try:
-        quantity = int(input("Input quantity: "))
-
-        print(f"You are about to add {quantity} order/s of {f.get(x)} to you Cart...")
-
-        y = input("Add to cart[y/n]?: ")
-
-        if y == "y":
-            Costs += quantity * f_p.get(x)
-            Items += quantity
-            List.append(f"{f.get(x)}({quantity})")
-
-            separator_line()
-
-            print(f"{quantity} order/s of {f.get(x)} added to Cart...")
-
-            menu()
-
-        elif y == "n":
-            separator_line()
-            print("\nOrder cancelled...\n")
-            ordering()
-
-        else:
-            input("--------------------------------------------------\n"
-                  "Invalid input... Press any key to continue...")
-            print("(Please use whole numbers for this input)"
-                  "-----------------------------------")
-            ordering()
-
-    except ValueError:
-        input("--------------------------------------------------\n"
-              "Invalid input... Press any key to continue...")
-        print("(Please use whole numbers for this input)"
-              "-----------------------------------")
-        ordering()
-
-# ----------------------------------------------------------------------
-'''Utility Functions'''
-
-def customer_cart():
-    if not List and not Items:
-        print("Your Cart:\n\n"
-              f"Cost: 0 php\n"
-              f"Items: none\n"
-              f"List: none\n\n"
-              f"Your balance: {Customer_Money} php\n")
-
-    else:
-        print("Your Cart:\n\n"
-              f"Cost: {Costs} php\n"
-              f"Items: {Items}\n"
-              f"List: {List}\n\n"
-              f"Your balance: {Customer_Money} php\n")
-
-
 def cart_menu():
     global Costs
     global Items
@@ -194,12 +99,16 @@ def cart_menu():
     if Items:
         customer_cart()
 
-        print("[1]Purchase [2]Reset [3]Exit")
+        print(f"Cart Cost: {Costs:n} php\n"
+              f"Delivery Cost: {Delivery_Cost} php \n"
+              f"Total Cost = {Customer_Money - Costs - Delivery_Cost:n} php\n\n"
+              "[1]Purchase [2]Reset [e]Exit")
 
         y = input("Choose option: ")
 
         if y == "1":
             if Customer_Money < (Costs + Delivery_Cost):
+                separator_line()
                 input("\nInsufficient payment...")
                 menu()
 
@@ -217,7 +126,7 @@ def cart_menu():
             List = []
             separator_line()
             input("My Cart has been reset... Press any key to continue...")
-            cart_menu()
+            menu()
 
         elif y == "3":
             menu()
@@ -227,15 +136,109 @@ def cart_menu():
             cart_menu()
 
     else:
-        input("There's currently nothing to purchase or reset in you Cart...\n"
-              "Press any key to continue...")
+        invalid_input()
+        cart_menu()
+
+def food_menu():
+    """Provides customer with a list of choices for food"""
+
+    separator_line()
+
+    print(f"Food Menu:\n"
+          f"[1]{f.get('f1')} = {f_p.get('f1'):n} php\n"
+          f"[2]{f.get('f2')} = {f_p.get('f2'):n} php\n"
+          f"[3]{f.get('f3')} = {f_p.get('f3'):n} php\n"
+          f"[4]{f.get('f4')} = {f_p.get('f4'):n} php\n"
+          f"[5]{f.get('f5')} = {f_p.get('f5'):n} php\n"
+          f"[e]Exit\n")
+
+    x = input("Choose option: ")
+
+    if x == "1":
+        transaction("f1")
+    elif x == "2":
+        transaction("f2")
+    elif x == "3":
+        transaction("f3")
+    elif x == "4":
+        transaction("f4")
+    elif x == "5":
+        transaction("f5")
+    elif x == "e":
+        print("(Exiting Food Menu...)")
         menu()
 
+    else:
+        invalid_input()
+        food_menu()
+
+def transaction(x):
+    """Make changes to COSTS/ITEMS/LIST in customer's CART based on his order"""
+    separator_line()
+
+    global Costs
+    global Items
+    global List
+
+    print(f"\n{f.get(x)} selected...\n")
+
+    try:
+        quantity = int(input("Input quantity: "))
+
+        if quantity > 1:
+            print(f"\nYou are about to add {quantity} orders of {f.get(x)} to you Cart...\n")
+        else:
+            print(f"\nYou are about to add {quantity} order of {f.get(x)} to you Cart...\n")
+
+        y = input("Add to cart[y/n]?: ")
+
+        if y == "y":
+            Costs += quantity * f_p.get(x)
+            Items += quantity
+            if not List:
+                List += f"{f.get(x)}({(str(quantity))})"
+            else:
+                List += f", {f.get(x)}({(str(quantity))})"
+            print(f"\n{quantity} order/s of {f.get(x)} added to Cart...\n")
+
+            menu()
+
+        elif y == "n":
+            separator_line()
+            print("\nOrder cancelled...\n")
+            food_menu()
+
+        else:
+            invalid_input()
+            food_menu()
+
+    except ValueError:
+        invalid_input()
+        separator_line()
+        food_menu()
+
+# ----------------------------------------------------------------------
+'''Utility Functions'''
+
+def customer_cart():
+    if not List and not Items:
+        f'{Customer_Money:n}'
+        print("Your Cart:\n\n"
+              f"Cost: ---\n"
+              f"Items: ---\n"
+              f"List: ---\n\n"
+              f"Your balance: {Customer_Money:n} php\n")
+
+    else:
+        print("Your Cart:\n\n"
+              f"Cost: {Costs:n} php\n"
+              f"Items: {Items}\n"
+              f"List: {List}\n\n"
+              f"Your balance: {Customer_Money:n} php")
 
 def invalid_input():
     separator_line()
     input("Invalid input... Press any key to continue...")
-
 
 def separator_line():
     print("----------------------------------------------------------")
